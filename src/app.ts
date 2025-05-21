a74d16c3152b926c55cad1c04990bd7c36fcd53d
Shai Gross <61117512+shaikegross@users.noreply.github.com>
2025-05-21 14:07:54 +0300

    Update app.ts
    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import db from './database';
+improt cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Fetch all users
 app.get('/users', (req, res) => {
